de59f559fdefff03c8ca30a683efc7efe91157b0
Khrystyna Lysiuk <khrystyna.lysiuk@irco.com>
2018-05-07 23:54:59 +0300

    timeline in progress

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Title from './components/Title.js'
 import colors from './configs/colors.js';
+import Timeline from './timeline/timeline.js';
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+    this.timelineArea = React.createRef();
+  }
+
+  createTimeline = (ev) => {
+    ev.preventDefault();
+
+    let timeline = new Timeline();
+
+    timeline.render({
+      parentDomElem: this.timelineArea.current,
+      startingPoint: 5,
+      height: '300'
+    });
+    
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <Title name="Hello" colors={Object.values(colors)}/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" ref={this.timelineArea}>  
+            <a href="#" onClick={this.createTimeline}>Create Timeline</a>   
       </div>
+
     );
   }
 }
---
+++ src/timeline/timeline.js
@@ -0,0 +1,96 @@
+/*
+
+class Timeline
+
+createTimeAxis
+
+addActivity
+
+addGroupActivity
+
+addPeriod
+
+
+*/
+import SVG from 'svg.js';
+
+export default class Timeline {
+	render({parentDomElem, startingPoint, height}) {
+		// TODO add checkings
+		this.createContainer(parentDomElem, height);	
+		this.createTimeAxis(startingPoint);
+		this.init();
+	}
+	init() {
+		this.clickListener = (function(ev) {
+			let x = this.svg.point(ev.clientX, ev.clientY).x;
+			let margin = this.margin;
+	
+			let quaterWidth = (this.width-margin*2)/24/4;
+			let quatersFromStart = (x-margin)/quaterWidth;
+			let currentHour = Math.floor(quatersFromStart/4) + this.startingPoint;
+			let minutes = (Math.floor(quatersFromStart)%4) * 15;
+			console.log(currentHour, minutes);
+
+
+		}).bind(this);
+		this.svg.node.addEventListener("click", this.clickListener, false);
+	}
+	createContainer(parentDomElem, height = 300) {
+		// set width and height
+		this.width = parentDomElem.clientWidth;
+		this.height = height;
+
+		this.domElem = document.createElement('div');
+		this.svg = SVG(this.domElem).size(this.width, this.height);
+
+		parentDomElem.appendChild(this.domElem);
+		// TODO maybe remove extra div
+	}
+	createTimeAxis(startingPoint = 5) {
+		this.startingPoint = startingPoint;
+
+		var timeAxis = this.svg.group();
+		timeAxis.addClass('time-axis');
+
+		const labelFontSize = this.height/25;
+		const margin = labelFontSize*1.5;
+		const quatersCount = 24*4;
+		const quater = (this.width-margin*2)/quatersCount;
+		const yStart = this.height/2; // y coord of tick
+		const yEnd = this.height/3; // y coord of small quoter tick
+		const yEndHour = this.height/4; // y coord of big hour tick
+
+		this.margin = margin;
+		console.log('margin ', this.margin);
+
+
+		for (let i = 0; i < quatersCount+1; i++) { // 23*4 = hours* quoters in hour
+			let isHourTick = (i == 0 || !(i%4));
+			
+			if (isHourTick) { // add label
+				let currentHour = (startingPoint + i/4)%24;
+				currentHour = currentHour < 10 ? `0${currentHour}` : currentHour;
+				
+				let text = timeAxis.text(`${currentHour}:00`);
+				text
+				.font({ fill: '#f06', family: 'Inconsolata', size: labelFontSize })
+				.attr({
+					x: i*quater + margin -(text.node.clientWidth/2),
+					y: yEndHour - labelFontSize*1.5,
+				});
+			}
+
+			timeAxis.line( // add tick
+				i*quater + margin,
+				yStart,
+				i*quater + margin,
+				(isHourTick ? yEndHour : yEnd)
+			).stroke({ width: 1 })
+		}
+	}
+	unrender() {
+		// TODO remove DOM elem
+		this.svg.removeEventListener("click", this.clickListener, false);
+	}
+}
